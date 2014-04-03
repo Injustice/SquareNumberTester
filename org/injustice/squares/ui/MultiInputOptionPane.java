c3f72264e80c62c182b6551d18c13d099880a063
Injustice <therealinjustice@gmail.com>
2014-04-03 19:28:40 +0100

    Support for checking input is valid range/type
    Throws an IllegalInputException if the input is text, negative, above
    the square root of Integer.MAX_VALUE or not an integer

--- org/injustice/squares/ui/MultiInputOptionPane.java
+++ org/injustice/squares/ui/MultiInputOptionPane.java
@@ -1,5 +1,6 @@
 package org.injustice.squares.ui;
 
+import org.injustice.squares.exceptions.IllegalInputException;
 import org.injustice.squares.handlers.Handler;
 
 import javax.swing.*;
@@ -19,18 +20,35 @@ public class MultiInputOptionPane extends JOptionPane {
         int result = JOptionPane.showConfirmDialog(null, panel,
                 "Square Number Tester", JOptionPane.OK_CANCEL_OPTION);
         if (result == JOptionPane.OK_OPTION) {
-            if (!option.getText().matches("^[a-zA-Z]+$")) {
-                System.out.println("Max number: " + option.getText());
-                handler.getDataHandler().setTotalNumberQuestions(Integer.parseInt(option.getText()));
-                handler.getDataHandler().setCorrectInFirstGo(handler.getDataHandler().getTotalNumberQuestions().intValue());
-            } else {
-                System.out.println("Making new");
+            try {
+                if (!option.getText().matches("^[a-zA-Z]+$")) {
+                    if (Double.parseDouble(option.getText()) ==
+                            (int) Double.parseDouble(option.getText())) {
+                        int number = (int) Double.parseDouble(option.getText());
+                        if (number > 0) {
+                            if (number <= 46340) {
+                                System.out.println("Max number: " + option.getText());
+                                handler.getDataHandler().setTotalNumberQuestions(number);
+                                handler.getDataHandler().setCorrectInFirstGo(handler.getDataHandler().getTotalNumberQuestions().intValue());
+                            } else {
+                                throw new IllegalInputException("Input may not be greater than 46350");
+                            }
+                        } else {
+                            throw new IllegalInputException("Input must be positive");
+                        }
+                    } else {
+                        throw new IllegalInputException("Input must be an integer");
+                    }
+                } else {
+                    throw new IllegalInputException("Input must be a number");
+                }
+            } catch (IllegalInputException e) {
+                e.printStackTrace();
                 new MultiInputOptionPane(handler);
             }
         } else if (result == JOptionPane.CANCEL_OPTION || result == JOptionPane.CLOSED_OPTION) {
             System.exit(0);
         } else {
-            System.out.println("wat");
             new MultiInputOptionPane(handler);
         }
     }
